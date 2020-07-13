1606e241b696b6ac2706bfed137b0f39040e9ab3
Kartik Kumar <58976221+Kartik-Kumar12@users.noreply.github.com>
2020-07-13 19:49:28 +0530

    initial commit

---
+++ assets/js/barchart.js
@@ -0,0 +1,10 @@
+!function(e){var t={};function r(a){if(t[a])return t[a].exports;var n=t[a]={i:a,l:!1,exports:{}};return e[a].call(n.exports,n,n.exports,r),n.l=!0,n.exports}r.m=e,r.c=t,r.d=function(e,t,a){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:a})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var a=Object.create(null);if(r.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var n in e)r.d(a,n,function(t){return e[t]}.bind(null,n));return a},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="/",r(r.s=404)}({404:function(e,t,r){e.exports=r(405)},405:function(e,t,r){"use strict";r.r(t);r(406)},406:function(e,t){!function(){"use strict";
+var e=function(e){
+
+   r=function(e,t,r){
+    t("#ordersChart"),function(e){
+      var t=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"doughnut",r=arguments.length>2&&void 0!==arguments[2]?arguments[2]:{},a={labels:["Desktop","Tablet","Mobile"],datasets:[{data:[60,25,15],backgroundColor:[],hoverBorderColor:settings.colors.white}]};Charts.create(e,t,r,a)
+    }
+
+
+}()}});
